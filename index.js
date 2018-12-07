18cb43dcf8e737e3f2f738ff02202dbcdad162a5
CodersTokyo <info@coders.tokyo>
2018-12-08 02:50:33 +0700

    Dog eat Cat, Cat eat Mouse

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Dog = require('./dog');
 const Cat = require('./cat');
 const Mouse = require('./mouse');
+const chalk = require('chalk');
 
 var jerry = new Mouse('jerry');
+var tom = new Cat('tom');
+var bull = new Dog('bull');
 jerry.running();
+tom.eat(jerry);
+console.log(tom);
+console.log(jerry);
+bull.eat(tom)
+console.log(bull);
+
